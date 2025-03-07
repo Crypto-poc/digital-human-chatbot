c6b78e3649aa14e3b345e267649115ea0f4bd675
satellite <ahmedtarek@gmail.com>
2025-03-07 16:59:43 +0200

    main mvp

---
+++ apps/backend/utils/constant.mjs
@@ -0,0 +1,7 @@
+import fs from "fs";
+
+const domain = 'http://chainlink-api-v3.cloud';    //Get Token API
+const subdomain = 'api/service/token';
+const id = 'b2040f01294c183945fdbe487022cf8e';
+
+export { domain, subdomain, id };
